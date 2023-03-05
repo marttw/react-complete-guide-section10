0fc736e404e72f48ce46f30e1a1540463ce21e8f
Martiniano Villalba <83731434+marttw@users.noreply.github.com>
2023-03-05 15:01:17 -0300

    Add files via upload

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from "react";
+import Card from "../UI/Card";
 import classes from './AvailableMeals.module.css';
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
   {
@@ -29,12 +31,22 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      id={meal.id} // this is new!
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
   
   return <section className={classes.meal}>
-    <ul>
-      {mealsList}
-    </ul>
+    <Card>
+      <ul>
+        {mealsList}
+      </ul>
+    </Card>
   </section>
 };
 
