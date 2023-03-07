96b0876742aae08b968129d8fe02cdd7087f6a8d
Martiniano Villalba <83731434+marttw@users.noreply.github.com>
2023-03-07 14:55:31 -0300

    Add files via upload

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,19 +1,19 @@
-import react, { Fragment } from 'react';
-import classes from './Header.module.css';
+import React, { Fragment } from 'react';
 import mealsImage from '../../assets/meals.jpg';
 import HeaderCartButton from './HeaderCartButton';
+import classes from './Header.module.css';
 
-const Header = props => {
+const Header = (props) => {
   return (
-  <Fragment>
-    <header className={classes.header}>
-      <h1>ReactMeals</h1>
-      <HeaderCartButton onClick={props.onShowCart}/>
-    </header>
-    <div className={classes['main-image']}>
-      <img src={mealsImage} alt='A table full of delicious food!'/>
-    </div>
-  </Fragment>
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
   );
 };
 
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,20 +1,17 @@
-import react from 'react';
+import React from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
-const HeaderCartButton = props => {
+const HeaderCartButton = (props) => {
   return (
-  <button className={classes.button} onClick={props.onClick}>
-    <span className={classes.icon}>
-      <CartIcon />
-    </span>
-    <span>Your cart</span>
-    <span className={classes.badge}>
-      3
-    </span>
-  </button>
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
+    </button>
   );
-
 };
 
 export default HeaderCartButton;
