a0ce20acf237df0cd7b9f81206b6293357d78343
Martiniano Villalba <mvillalba@HEXACTA.COM>
2023-03-08 17:02:36 -0300

    class 144 completed

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmounIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = event => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (enteredAmount.trim().lenth === 0 || enteredAmount < 1 || enteredAmount > 5) {
+      setAmounIsValid(false);      
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
      <Input
+      ref={amountInputRef}
       label='Amount'
       input={{
           id: 'amount_' + props.id, // this changed!
@@ -17,6 +35,7 @@ const MealItemForm = (props) => {
       }}
     />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
